32e23a4b419cf95acc3fb136a41ed760ae9f5277
kbrowns <kelly.brownsberger@gmail.com>
2013-10-14 12:53:43 -0400

    forgot class diagram factory

---
+++ Src/NPlant/ClassDiagramFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace NPlant
+{
+    internal abstract class DiagramFactory<T> : IDiagramFactory where T : IDiagram
+    {
+        public abstract IEnumerable<IDiagram> GetDiagrams();
+    }
+}
