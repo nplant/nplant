e4264ad836aeb439455dd07ad2538ccb5511c058
Kelly Brownsberger <kelly.brownsberger@csgi.com>
2014-08-26 11:21:19 -0400

    adding bidirectional sample

---
+++ Src/NPlant.Samples/Relationships/BidirectionalAssociationDiagram.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace NPlant.Samples.Relationships
+{
+    public class BidirectionalAssociationDiagram : ClassDiagram
+    {
+        public BidirectionalAssociationDiagram()
+        {
+            base.AddClass<Order>();
+        }
+    }
+
+    public class Order
+    {
+        public long Id;
+        public IList<OrderItem> Items;
+    }
+
+    public class OrderItem
+    {
+        public long Id;
+        public Order Order;
+        public Product Product;
+        public Price Price;
+    }
+
+    public class Product
+    {
+        public long Id;
+        public string Name;
+        public string Description;
+    }
+
+    public class Price
+    {
+        public long Id;
+        public string Name;
+        public string Description;
+        public decimal Amount;
+    }
+}
