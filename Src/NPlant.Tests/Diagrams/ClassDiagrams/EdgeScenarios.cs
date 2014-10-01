035b197cda1e45db738c656a79dd17d37b38e550
Kelly Brownsberger <kelly.brownsberger@csgi.com>
2014-10-01 19:16:45 -0400

    cleaning up nuget package references removing the old msbuild based package restore cruft nuking the nant task that was temporarily created for gen'ing the samples manifest

--- Src/NPlant.Tests/Diagrams/ClassDiagrams/EdgeScenarios.cs
+++ Src/NPlant.Tests/Diagrams/ClassDiagrams/EdgeScenarios.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using NPlant.MetaModel.ClassDiagraming;
-using NPlant.Samples.CIrcularReferfences;
+using NPlant.Samples.CircularReferences;
 using NUnit.Framework;
 
 namespace NPlant.Tests.Diagrams.ClassDiagrams
 {
     [TestFixture]
-    public class EdgeScenarios
+    public class EdgetScenarios
     {
         [Test]
         public void Circular_References_Dont_Create_Multiple_Classes()
         {
-            var simulation = new ClassDiagramSimulation(new ClassDiagram(typeof(Foo)));
-            simulation.Simulate();
+            var simulation = new SimulatedClassDiagramGenerator(new ClassDiagram(typeof(Foo)));
+            simulation.Generate();
 
             Assert.That(simulation.Classes.Count, Is.EqualTo(3));
             Assert.That(simulation.Classes["Foo"].Members["SomeString"].MemberType, Is.EqualTo(typeof(string)));
@@ -23,4 +23,5 @@ namespace NPlant.Tests.Diagrams.ClassDiagrams
             Assert.That(simulation.Classes["Baz"].Members["TheFoo"].MemberType, Is.EqualTo(typeof(Foo)));
         }
     }
+
 }
