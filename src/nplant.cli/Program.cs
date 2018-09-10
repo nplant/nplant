694e70c7dbea1ae7278a2a2f02f500ee6fa0f372
brok02 <kelly.brownsberger@csgi.com>
2018-09-10 14:28:37 -0400

    skeleton projects for lib, cli, test

---
+++ src/nplant.cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nplant.cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/nplant.unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace nplant.unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
